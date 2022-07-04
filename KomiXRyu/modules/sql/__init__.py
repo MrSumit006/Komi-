a7271dafcfaa97f8a16634fc594331d58ed7a33d
MrSumit006 <sumitsenpai004@gmail.com>
2022-07-04 09:48:27 +0530

    Update __init__.py

--- KomiXRyu/modules/sql/__init__.py
+++ KomiXRyu/modules/sql/__init__.py
@@ -5,13 +5,17 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from KomiXRyu import DB_URI, LOGGER
 
 
-
 def start() -> scoped_session:
-    engine = create_engine(DB_URL, client_encoding="utf8")
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    LOGGER.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION = start()
+except Exception as e:
+    LOGGER.exception(f"[PostgreSQL] Failed to connect due to {e}")
+    exit()
